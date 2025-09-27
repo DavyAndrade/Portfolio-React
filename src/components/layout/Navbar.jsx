9522bab5750aabf6d0117a235c731cf060a3b12b
DavyAndrade <davy.dama55@gmail.com>
2025-09-27 00:29:48 -0300

    first commit

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <header className="">
+        <h2>Davy Andrade</h2>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#home">Início</a>
+            </li>
+            <li>
+              <a href="#about">Sobre</a>
+            </li>
+            <li>
+              <a href="#skills">Habilidades</a>
+            </li>
+            <li>
+              <a href="#projects">Projetos</a>
+            </li>
+            <li>
+              <a href="#contact">Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
