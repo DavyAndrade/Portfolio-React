929bd1668fa2aff8b493fb1a9c28daa141c0c8f1
DavyAndrade <davy.dama55@gmail.com>
2025-10-01 18:36:32 -0300

    feat(Projects): Add useEffect to set limit by screen size
    Limitando a quantidade de projetos exibidos a depender da tela do usuário e ajuste conforme resize.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Hero from "./components/sections/Hero/Hero";
-import About from "./components/sections/About/About";
-import Skills from "./components/sections/Skills/Skills";
-import Projects from "./components/sections/Projects/Projects";
-import Contact from "./components/sections/Contact/Contact";
-import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar.jsx";
+import Hero from "./components/sections/Hero/Hero.jsx";
+import About from "./components/sections/About/About.jsx";
+import Skills from "./components/sections/Skills/Skills.jsx";
+import Projects from "./components/sections/Projects/Projects.jsx";
+import Contact from "./components/sections/Contact/Contact.jsx";
+import Footer from "./components/layout/Footer.jsx";
 
 function App() {
   return (
--- src/components/sections/Projects/ProjectsContainer.jsx
+++ src/components/sections/Projects/ProjectsContainer.jsx
@@ -1,26 +1,33 @@
 // Import do useState
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Import dos componentes
 import ProjectCard from "./ProjectCard.jsx";
 import Button from "../../ui/Button.jsx";
 
-// import dos projetos
-import projects from "./projects.js";
-
-// import das imagens
-import FitCheckImg from "../../../assets/fitcheck.png";
-import TaskFlowImg from "../../../assets/taskflow.png";
-import LocalizaCEPImg from "../../../assets/localizacep.png";
-import BentoGridImg from "../../../assets/bentogrid.png";
-import SocialLinkProfileImg from "../../../assets/social-links.png";
-
 // import dos ícones
 import { IoArrowDownCircleOutline } from "react-icons/io5";
 import { IoArrowUpCircleOutline } from "react-icons/io5";
 
+import projects from "./projects.js";
+
 export default function ProjectContainer() {
+  const getInitialLimit = () => {
+    if (window.innerWidth < 1024) return 2;
+    return 3;
+  };
+
   const [showAll, setShowAll] = useState(false);
+  const [limit, setLimit] = useState(getInitialLimit());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setLimit(getInitialLimit());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const toggleShowAll = () => {
     setShowAll(!showAll);
@@ -34,7 +41,7 @@ export default function ProjectContainer() {
     }
   };
 
-  const displayProjects = showAll ? projects : projects.slice(0, 3);
+  const displayProjects = showAll ? projects : projects.slice(0, limit);
 
   return (
     <div className="flex flex-col items-center justify-center gap-8 w-full">
@@ -44,10 +51,12 @@ export default function ProjectContainer() {
         ))}
       </div>
 
-      {projects.length > 3 && (
+      {projects.length > limit && (
         <Button
           action={toggleShowAll}
-          text={showAll ? "Ver Menos" : `Ver Mais (${projects.length - 3})`}
+          text={
+            showAll ? "Ver Menos" : `Ver Mais (${projects.length - limit})`
+          }
           icon={
             showAll ? <IoArrowUpCircleOutline /> : <IoArrowDownCircleOutline />
           }
