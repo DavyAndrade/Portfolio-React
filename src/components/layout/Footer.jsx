5a9e995eb732f831a97da79ca5af955e9f3dfe24
DavyAndrade <davy.dama55@gmail.com>
2025-10-02 17:42:49 -0300

    fix(Footer): Corrigindo cores dos icons
    Agora, com hover, altera a cor do icon corretamente.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -11,31 +11,31 @@ export default function Footer() {
         </h3>
 
         <ul className="flex gap-6">
-          <li className="bg-emerald-500 w-10 h-10 rounded-full flex items-center justify-center hover:bg-emerald-600 text-black hover:text-gray-300 transition-colors">
+          <li className="bg-emerald-500 w-10 h-10 rounded-full flex items-center justify-center hover:bg-emerald-600 transition-colors">
             <a
               href="https://www.linkedin.com/in/davy-andrade"
               target="_blank"
-              className="text-2xl p-2 rounded-full"
+              className="text-2xl p-2 rounded-full text-black hover:text-gray-300 transition-colors"
             >
               <FaLinkedin />
             </a>
           </li>
-          <li className="bg-sky-500 w-10 h-10 rounded-full flex items-center justify-center hover:bg-sky-600 text-black hover:text-gray-300 transition-colors">
+          <li className="bg-sky-500 w-10 h-10 rounded-full flex items-center justify-center hover:bg-sky-600 transition-colors">
             <a
               href="https://github.com/DavyAndrade"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-2xl p-2 rounded-full"
+              className="text-2xl p-2 rounded-full text-black hover:text-gray-300 transition-colors"
             >
               <FaGithub />
             </a>
           </li>
-          <li className="bg-gradient-to-r from-emerald-500 to-sky-500 w-10 h-10 rounded-full flex items-center justify-center hover:opacity-80 text-black hover:text-gray-300 transition-colors">
+          <li className="bg-gradient-to-r from-emerald-500 to-sky-500 w-10 h-10 rounded-full flex items-center justify-center hover:opacity-80 transition-colors">
             <a
               href="https://twitter.com/davy_andrade"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-2xl p-2 rounded-full"
+              className="text-2xl p-2 rounded-full text-black hover:text-gray-300 transition-colors"
             >
               <FaXTwitter />
             </a>
